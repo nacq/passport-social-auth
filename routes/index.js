2d1858c267b05e85a52acd8874753e0b7c33ff17
Nicolas Acquaviva <nicoacq@gmail.com>
2016-12-23 17:38:29 -0300

    google oauth2 strategy

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var passport = require('passport');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/auth/google', passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), function(req, res) {
+  console.log(res.req.user);
+  res.render('index', { title: 'Express', userData: res.req.user.accessToken });
+});
+
+module.exports = router;
